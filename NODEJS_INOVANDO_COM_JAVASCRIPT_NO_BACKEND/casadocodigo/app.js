0233a5301b50ec2c0b2db26efcbf02e07e9061d4
marcelovbm <marcelo.magrinelli@equals.com.br>
2017-09-02 13:19:02 -0300

    Fim da Primeira Atividade

--- NODEJS_INOVANDO_COM_JAVASCRIPT_NO_BACKEND/casadocodigo/app.js
+++ NODEJS_INOVANDO_COM_JAVASCRIPT_NO_BACKEND/casadocodigo/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/produtos', function(req, res) {
-    res.send('<html><body>Request recebido!</body></html>');
+    res.render('produtos/lista');
 });
 
 app.listen(3000, function() {
