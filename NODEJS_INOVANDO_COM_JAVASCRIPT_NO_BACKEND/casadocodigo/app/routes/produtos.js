e9b1802baceb4d857b3aead09b1118522573fc14
marcelovbm <marcelo@gmail.com>
2017-09-07 21:17:43 -0300

    Exibindo na tela os dados do Banco

--- NODEJS_INOVANDO_COM_JAVASCRIPT_NO_BACKEND/casadocodigo/app/routes/produtos.js
+++ NODEJS_INOVANDO_COM_JAVASCRIPT_NO_BACKEND/casadocodigo/app/routes/produtos.js
@@ -3,6 +3,27 @@ module.exports = function (app) {
     console.log("Modulo de Rotas dos Produtos carregada!");
     app.get('/produtos', function(req, res) {
         var mysql = require('mysql');
-        res.render('produtos/lista');
+
+        console.log('listando...');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '',
+            database :  'casadocodigo_nodejs'
+        });
+        
+        connection.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+          });
+
+        connection.query('select * from produtos', function(err, results){
+            // res.send(results);
+            res.render('produtos/lista',{lista:results});
+        });
+
+        connection.end();
+        //res.render('produtos/lista');
     });
 }
